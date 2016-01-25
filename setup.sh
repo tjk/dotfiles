3be30948aba86aadb99f769d604599be6f3f8d37
TJ Koblentz <tj.koblentz@gmail.com>
2016-01-24 23:01:21 -0800

    remove vimerc and useless rm protection

--- setup.sh
+++ setup.sh
@@ -2,18 +2,12 @@
 #
 # Assumes git is installed
 
-# https://news.ycombinator.com/item?id=9255200
-# "When accidentally running rm -f *, the command expands to -@ first, which is
-# not a valid option and makes the command fail before doing any harm"
-touch ~/-@
-sudo touch /-@
-
 # Determine current directory
 # http://stackoverflow.com/a/4774063
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # Move dotfiles to $HOME directory
-files=(vimrc vimerc aliases gitignore gitconfig)
+files=(vimrc aliases gitignore gitconfig)
 for file in ${files[@]}
 do
   ln -vs $SCRIPTPATH/.$file $HOME/.$file
