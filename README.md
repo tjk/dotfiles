6123d87dbe3f78f4f495594da10c37ce4df686e7
TJ Koblentz <tj.koblentz@gmail.com>
2021-12-29 10:12:42 -0800

    save

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ README
 
 Configuration stuff -- take a look if you'd like. :)
 
-TODO:
-- https://github.com/anishathalye/dotbot
+```
+% # install stow
+% ./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-#
-# Assumes git is installed
-
-# Determine current directory
-# http://stackoverflow.com/a/4774063
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-# Move dotfiles to $HOME directory
-files=(vimrc aliases gitignore gitconfig)
-for file in ${files[@]}
-do
-  rm $HOME/.$file
-  ln -vs $SCRIPTPATH/.$file $HOME/.$file
-done
-
-# -- swap directory
-mkdir -p $HOME/.vim/swap
+#!/bin/sh -ex
+stow --restow --target=$HOME git
+stow --restow --target=$HOME nvim
