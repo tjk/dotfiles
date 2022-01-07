64ae2a7152ac9f767bc2675dfd1898a7e2a139fe
TJ Koblentz <tj.koblentz@gmail.com>
2022-01-07 10:29:59 -0800

    add some oh-my-zsh setup

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ README
 
 ## Resources
 
-- ohmyzsh setup:
+- oh-my-zsh setup:
   - https://gist.github.com/tjk/10d6b1aea1cd202a88abf19f4cbb05f8
 - auto attach to tmux on iterm2 start:
   - https://coderwall.com/p/-mumdg/automatically-reattach-tmux-session-using-iterm2
--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ stow --restow --target=$HOME git
 stow --restow --target=$HOME nvim
 stow --restow --target=$HOME tmux
 stow --restow --target=$HOME zsh
+[ ! -d $HOME/.oh-my-zsh ] && curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+# TODO
+echo "brew install zsh-syntax-highlighting"
