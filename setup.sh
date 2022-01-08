1f2ec6b5b204e9d917cd69b77f6bf51aeef5f894
TJ Koblentz <tj.koblentz@gmail.com>
2022-01-08 10:31:03 -0800

    improvements to zsh setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ stow --restow --target=$HOME git
 stow --restow --target=$HOME nvim
 stow --restow --target=$HOME tmux
 stow --restow --target=$HOME zsh
-[ ! -d $HOME/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ ! -d $HOME/.oh-my-zsh ] && CHSH=no RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+command -v zsh | sudo tee -a /etc/shells
+sudo chsh -s "$(command -v zsh)" "${USER}"
 # TODO
 echo "brew install zsh-syntax-highlighting"
