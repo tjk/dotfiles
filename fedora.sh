bf3a907ceb0b5b723e36fe635921180228c39e66
TJ Koblentz <tj.koblentz@gmail.com>
2024-07-17 10:00:52 -0700

    FiraCode font, gruvbox sway + wallpaper change

--- fedora.sh
+++ fedora.sh
@@ -16,8 +16,13 @@ pkgs+=("eza" "fzf" "neovim" "zoxide")
 pkgs+=("sway" "waybar" "rofi" "network-manager-applet")
 # for controlling via sway XF86 keys
 pkgs+=("brightnessctl" "pulseaudio-utils")
+# XXX: cannot get this working... maybe need perms? (TODO: maybe try gammastep ?)
+pkgs+=("redshift")
 pkgs+=("google-chrome-stable")
 # XXX: check whether golang-x-tools-gopls is included in go
 pkgs+=("go" "golang-x-tools-gopls")
 
 sudo dnf -y install ${pkgs[*]}
+
+mkdir -p ~/.local/share/fonts
+ls ~/.local/share/fonts/FiraCodeNerdFont-Regular.ttf >/dev/null 2>/dev/null || (cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf)
