6ae511f19385734310ff7ff0cac19b09d732c227
TJ Koblentz <tj.koblentz@gmail.com>
2024-08-15 20:38:52 -0700

    fish: keychain eval + some var changes that shouldnt be in git

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,5 @@ if status is-interactive
   source ~/.asdf/asdf.fish
   # add this for pip
   fish_add_path ~/.asdf/installs/python/3.10.5/bin
+  eval (keychain --eval --agents ssh -Q --quiet id_ed25519 --nogui)
 end
